26ec78e7a3089d53969ec39d01b529f0eacf6021
Prasant Paudel <prashant.paudel.555@gmail.com>
2020-05-21 06:44:38 +0545

    project finished ;)

--- jobs/models.py
+++ jobs/models.py
@@ -3,5 +3,8 @@ from django.db import models
 # Create your models here.
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
+    # name = models.CharField(max_length=100)
     summary = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.summary
