602a4fe09c0b3a2b44ad4856aabf28fd510627b8
Prasant Paudel <prashant.paudel.555@gmail.com>
2020-05-23 18:11:18 +0545

    whitenoise middleware added for serving static files

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -37,11 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'jobs'
+    'jobs',
+    'storages'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
